152fd4217688e8500bc48f622ed07f669705b537
fmwong77 <fongmeiw@gmail.com>
2019-12-16 21:44:25 -0600

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -2,10 +2,6 @@
 class BirdsController < ApplicationController
   def index
     @birds = Bird.all
-    render json: {birds: @birds, message: ['Hello Birds', 'Goodbye Birds']} 
-    # render json: 'Remember that JSON is just object notation converted to string data, so strings als work here'
-    # render json: ['an', 'array', 'too']
-    # render plain: "Hello #{@birds[3].name}"
-    # render 'birds/index.html.erb'
+    render 'birds/index.html.erb'
   end
 end
